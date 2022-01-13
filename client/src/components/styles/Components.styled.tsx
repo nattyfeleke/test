6a9e8e182eec722ed856927acf61fa29ad048da6
Natnael Feleke <nfeleke568@gmail.com>
2022-01-13 11:30:07 +0300

    fixed some styles

--- client/src/components/styles/Components.styled.tsx
+++ client/src/components/styles/Components.styled.tsx
@@ -68,10 +68,11 @@ export const RadioButtonInput = styled.input`
 `;
 
 export const Options = styled.div`
-display: flex,
-align-items: center;
-gap: 0.25rem;
-margin: 2px 0`;
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  margin: 2px 0;
+`;
 
 export const FormGroup = styled.div`
   margin: 20px 0;
