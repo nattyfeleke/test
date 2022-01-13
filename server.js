8f8048cf2fa51ac9f1d92e93dd518dbcd7decd76
Natnael Feleke <nfeleke568@gmail.com>
2022-01-13 10:30:51 +0300

    heoku prod

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/api/employees', require('./routes/api/employees'));
 
 console.log(process.env.NODE_ENV);
-const PORT = process.env.NODE_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 console.log(PORT);
 
